d12bf428e648944afd36da1423bb42fe0cc9b12b
mirat <mmirat218@gmail.com>
2022-08-10 19:31:13 +0600

    Created a project and the application did everything as it should

--- project/settings.py
+++ project/settings.py
@@ -12,10 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-$k_67ux0dhiob+xbi=-^x+^t22*a!edhwaiufbiwbf'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1']
 
 
